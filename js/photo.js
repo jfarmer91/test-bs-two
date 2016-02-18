d021ad6380259da458ef7b077f3ab9946f084ce9
jfarmer91 <farmerjong@gmail.com>
2016-02-18 12:12:24 -0500

    Working with gallery

--- js/photo.js
+++ js/photo.js
@@ -3,15 +3,24 @@ document.addEventListener('load', imageAutoSwipe());
 function imageAutoSwipe() {
   var xhttp = new XMLHttpRequest();
   // Send XMLHttpRequest
-  xhttp.open("GET", 'gallery', true);
+  xhttp.open("GET", 'https://api.github.com/repos/jfarmer91/test-bs-two/contents/gallery/', true);
   xhttp.send();
 
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       console.log(xhttp);
-      console.log(xhttp.responseText);
-      // var x = JSON.parse(xhttp.responseText);
-      // console.log(x);
+      var imageObject = JSON.parse(xhttp.responseText);
+      console.log(imageObject);
+
+      var carouselIndicators = '';
+
+      var carouselInner = '';
+
+      for ( i = 0; i < imageObject.length; i++ ) {
+        carouselIndicators += "<li data-target='#myCarousel' data-slide-to='" + i + "' class='active'></li>";
+
+        carouselInner += "<div class='item active'><img src='" + imageObject[i].name + "' alt='site-image' width='460' height='345'></div>"
+      }
     }
   }
 
